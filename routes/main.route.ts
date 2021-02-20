8966e27af62273510f467eca6830372a07f4086b
Mac <mac@Arsls-MacBook-Pro.local>
2021-02-20 15:46:35 +0500

    heroku

--- routes/main.route.ts
+++ routes/main.route.ts
@@ -6,14 +6,6 @@ import authGuard from './authGuard.route';
 
 const router = express.Router();
 
-router.get('/',(req,res)=> {
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
-    res.send('hello from Server');
-});
 
 router.use('/todo', todoRouter);
 router.use('/user', UserRouter);
